45c1467c846518d6ea90aaf631fc938f3c5829e6
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-16 06:30:23 -0500

    An NgFor sentence was created. The class: 'NgFor aplicado en la lista de usuarios' has finished.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../interfaces/user';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  friends: User[];
+
+  constructor() {
+    let users : User[]= [
+      { uid: "1",  nick: 'Eduardo', subnick: 'Mi mensaje personal',  age: 28, email: 'eduardo@platzi.com', friend: true},
+      { uid: "2", nick: 'Yuliana', subnick: 'Mi mensaje personal', age: 25, email: 'yuliana@platzi.com', friend: true},
+      { uid: "3", nick: 'Freddy', subnick: 'Mi mensaje personal', age: 28, email: 'freddy@platzi.com', friend: false}
+    ];
+    
+    this.friends = users
+
+   }
 
   ngOnInit(): void {
   }
