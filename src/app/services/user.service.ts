a20d5229b2da779e1e384c94dde5a1f0e4ab28e2
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-16 12:54:07 -0500

    Icons to the status of the friend were added.

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -9,9 +9,10 @@ export class UserService {
   friends: User[];
   constructor() {
     let users : User[]= [
-      { uid: "1",  nick: 'Eduardo', subnick: 'Mi mensaje personal',  age: 28, email: 'eduardo@platzi.com', friend: true},
-      { uid: "2", nick: 'Yuliana', subnick: 'Mi mensaje personal', age: 25, email: 'yuliana@platzi.com', friend: true},
-      { uid: "3", nick: 'Freddy', subnick: 'Mi mensaje personal', age: 28, email: 'freddy@platzi.com', friend: false}
+      { uid: "1",  nick: 'Eduardo', subnick: 'Mi mensaje personal',  age: 28, email: 'eduardo@platzi.com', friend: true, status: "busy"},
+      { uid: "2", nick: 'Yuliana', subnick: 'Mi mensaje personal', age: 25, email: 'yuliana@platzi.com', friend: true, status: "online"},
+      { uid: "3", nick: 'Freddy', subnick: 'Mi mensaje personal', age: 28, email: 'freddy@platzi.com', friend: false, status: "offline"},
+      { uid: "4", nick: 'Cl82', subnick: 'Mi mensaje personal', age: 23, email: 'clas82@platzi.com', friend: false, status: "away"}
     ];
     
     this.friends = users
