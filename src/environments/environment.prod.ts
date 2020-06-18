74d4b4b88f588cfe03f099117fff3a3fd883c68b
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-18 09:31:26 -0500

    The mdependency of Firebase was added. The class: 'Instalacion y set up de la libreria AngularFire a traves de npm' has finished.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBu75kJrFFtVLil6xtUlCXuD0KlMyjdYCY",
+    authDomain: "platzinger-a833c.firebaseapp.com",
+    databaseURL: "https://platzinger-a833c.firebaseio.com",
+    projectId: "platzinger-a833c",
+    storageBucket: "platzinger-a833c.appspot.com",
+    messagingSenderId: "462612072009",
+    appId: "1:462612072009:web:0ef28759f1ec3c04d5a198"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBu75kJrFFtVLil6xtUlCXuD0KlMyjdYCY",
+    authDomain: "platzinger-a833c.firebaseapp.com",
+    databaseURL: "https://platzinger-a833c.firebaseio.com",
+    projectId: "platzinger-a833c",
+    storageBucket: "platzinger-a833c.appspot.com",
+    messagingSenderId: "462612072009",
+    appId: "1:462612072009:web:0ef28759f1ec3c04d5a198"
+  }
 };
 
 /*
