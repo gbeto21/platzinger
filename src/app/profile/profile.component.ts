52922cf1ff99f20dd8778d75bae5d08e4441eb14
Alberto González Balderas <albertoglezb@hotmail.com>
2020-06-18 16:53:17 -0500

    The funtionality to validate the status of the sessio was created. The class: 'Perfil del usuario' has finished.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../interfaces/user';
+import { UserService } from '../services/user.service';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+
+  constructor(
+    private userService: UserService,
+    private autenteticationService: AuthenticationService) 
+  {
+    this.autenteticationService
+      .getStatus()
+      .subscribe(
+        (status)=>{
+          this.userService.getUserById(status.uid)
+            .valueChanges()
+            .subscribe(
+              (data:User)=>{
+                this.user=data
+                console.log("DatosUsuario" )
+                console.log(this.user);
+                
+              }, 
+              (error)=>{
+                console.log(error);
+              }
+            )
+        },
+        (error)=>{
+          console.log(error);
+        }
+      )
+  }
 
   ngOnInit(): void {
+  }
+
+  saveSettings(){
+    this.userService.editUser(this.user)
+    .then(()=>{
+      alert('Cambios guardados')
+    })
+    .catch((err)=>{
+      alert('Error guardando cambios')
+      console.log(err);
+      
+    })
   }
 
 }
